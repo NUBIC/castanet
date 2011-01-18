14f81fcd795f93713db7c656b655cbff078a1f3a
David Yip <yipdw@member.fsf.org>
2011-01-17 22:06:11 -0600

    First cut at Castanet::Client.

--- lib/castanet.rb
+++ lib/castanet.rb
@@ -1,3 +1,3 @@
 module Castanet
-
+  autoload :Client, 'castanet/client'
 end
---
+++ lib/castanet/client.rb
@@ -0,0 +1,40 @@
+require File.join(File.dirname(__FILE__), %w(.. castanet))
+
+module Castanet
+  ##
+  # A CAS client.
+  #
+  # @see http://www.jasig.org/cas/protocol CAS protocol
+  #
+  # Validating a service ticket
+  # ===========================
+  #
+  #     client.valid_ticket?(ticket)  # => true or false
+  #
+  # `ticket` should be the token provided by the CAS server.
+  class Client
+    ##
+    # The URL of the CAS server.
+    #
+    # @return [String]
+    attr_accessor :cas_url
+
+    ##
+    # The ticket validator to use.
+    #
+    # The default validator is sufficient for most purposes; however, if you
+    # need to do testing against {Client}, you may find it useful to be able to
+    # control the behavior of the validator.
+    #
+    # @return [TicketValidator]
+    attr_accessor :ticket_validator
+
+    def initialize(settings = {})
+      self.cas_url = settings[:cas_url]
+    end
+
+    def valid_ticket?(ticket)
+      ticket_validator.valid?(ticket)
+    end
+  end
+end
