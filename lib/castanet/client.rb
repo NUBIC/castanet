9110c9b2cf1a3ffdc872198ff4e8945ad80ef29e
David Yip <yipdw@member.fsf.org>
2011-01-21 10:39:09 -0600

    First cut at service ticket validation.
    Well, the network bits.  The validation logic hasn't been written yet.

--- lib/castanet/client.rb
+++ lib/castanet/client.rb
@@ -1,5 +1,8 @@
 require File.join(File.dirname(__FILE__), %w(.. castanet))
 
+require 'net/http'
+require 'uri'
+
 module Castanet
   ##
   # A CAS client.
@@ -9,16 +12,10 @@ module Castanet
   # Validating a service ticket
   # ===========================
   #
-  #     client.valid_ticket?(ticket)  # => true or false
+  #     client.valid_ticket?(ticket, service_url)  # => truthy or false
   #
-  # `ticket` should be the token provided by the CAS server.
+  # `ticket` is the service ticket to validate.  `service` is the service URL.
   class Client
-    ##
-    # The URL of the CAS server.
-    #
-    # @return [String]
-    attr_accessor :cas_url
-
     ##
     # The ticket validator to use.
     #
@@ -29,18 +26,84 @@ module Castanet
     # @return [TicketValidator]
     attr_accessor :ticket_validator
 
+    ##
+    # The CAS server's URL.
+    #
+    # The URL must be terminated with a trailing slash if it contains a non-root
+    # mount point.
+    #
+    # @see http://www.ietf.org/rfc/rfc3986.txt RFC 3986 (URI syntax)
+    # @return [String, nil]
+    attr_accessor :cas_url
+
+    ##
+    # The URL of the proxy callback.
+    #
+    # @see http://www.jasig.org/cas/protocol CAS protocol, section 2.5.4
+    # @return [String, nil]
+    attr_accessor :proxy_callback_url
+
     def initialize(settings = {})
       self.cas_url = settings[:cas_url]
+      self.proxy_callback_url = settings[:proxy_callback_url]
     end
 
     ##
-    # Returns whether or not `ticket` is a valid service ticket for `service`.
+    # Returns the service ticket validation endpoint for the configured CAS URL.
+    #
+    # The service ticket validation endpoint is defined as {#cas_url} +
+    # `"/serviceValidate"`.
+    #
+    # @see CAS 2.0 protocol, section 2.5
+    # @see #cas_url
+    # @return [String]
+    def service_validate_url
+      URI.join(cas_url, 'serviceValidate').normalize.to_s
+    end
+
+    ##
+    # Returns whether or not `ticket` is a valid service ticket for `service`,
+    # as determined by {#ticket_validator}.
+    #
+    # If {#ticket_validator} also returns a PGT, then this method returns an
+    # array of two arguments: `true` followed by the PGT as a String.
     #
     # @param [String] ticket a service ticket
     # @param [String] service a service URL
-    # @return [Boolean]
+    # @return [Boolean] if the service ticket is valid
+    # @return [[true, String]] if the service ticket is valid and a PGT was
+    #   supplied
     def valid_ticket?(ticket, service)
-      ticket_validator.valid?(ticket, service)
+      uri = URI.parse(service_validate_url).tap do |u|
+        u.query = validation_parameters(ticket, service)
+      end
+
+      http = Net::HTTP.new(uri.host, uri.port).tap do |h|
+        h.use_ssl = (uri.scheme == 'https')
+      end
+
+      http.start do |h|
+        cas_response = h.get(uri.to_s)
+
+        ticket_validator.valid?(cas_response.body)
+      end
+    end
+
+    private
+
+    ##
+    # Builds a query string for use with serviceValidate.
+    #
+    # @see http://www.jasig.org/cas/protocol CAS protocol, section 2.5.1
+    # @param [String] ticket a service ticket
+    # @param [String] service a service URL
+    # @return [String] a query component of a URI
+    def validation_parameters(ticket, service)
+      [
+        [ 'ticket',   ticket ],
+        [ 'service',  service ],
+        [ 'pgtUrl',   proxy_callback_url ]
+      ].reject { |_, v| !v }.map { |x, y| URI.encode(x) + '=' + URI.encode(y) }.join('&')
     end
   end
 end
--- spec/castanet/client_spec.rb
+++ spec/castanet/client_spec.rb
@@ -6,41 +6,111 @@ module Castanet
 
     describe '#initialize' do
       it "sets the CAS server's URL" do
-        client = Client.new(:cas_url => 'https://cas.example.edu')
+        client = Client.new(:cas_url => 'https://cas.example.edu/')
 
-        client.cas_url.should == 'https://cas.example.edu'
+        client.cas_url.should == 'https://cas.example.edu/'
+      end
+
+      it 'sets the proxy callback URL' do
+        client = Client.new(:proxy_callback_url => 'https://cas.example.edu/proxy/')
+
+        client.proxy_callback_url.should == 'https://cas.example.edu/proxy/'
       end
     end
 
-    describe '#cas_url' do
-      it 'is settable' do
-        client.cas_url = 'https://cas.example.edu'
+    describe '#cas_url=' do
+      it 'stores the given URL' do
+        client.cas_url = 'https://cas.example.edu/cas/'
 
-        client.cas_url.should == 'https://cas.example.edu'
+        client.cas_url.should == 'https://cas.example.edu/cas/'
+      end
+    end
+
+    describe '#proxy_callback_url=' do
+      it 'stores the given URL' do
+        client.proxy_callback_url = 'https://cas.example.edu/proxy/'
+
+        client.proxy_callback_url.should == 'https://cas.example.edu/proxy/'
+      end
+    end
+
+    describe '#service_validate_url' do
+      it 'is the CAS server path plus "/serviceValidate"' do
+        client.cas_url = 'https://cas.example.edu/cas/'
+
+        client.service_validate_url.should == 'https://cas.example.edu/cas/serviceValidate'
+      end
+
+      it 'normalizes the generated URL' do
+        client.cas_url = 'hTTps://cas.example.edu/cas/'
+
+        client.service_validate_url.should == 'https://cas.example.edu/cas/serviceValidate'
       end
     end
 
     describe '#valid_ticket?' do
-      describe 'given a service ticket' do
-        let(:validator) { mock }
-        let(:ticket) { 'ST-1foo' }
-        let(:service) { 'https://service.example.edu' }
+      let(:validator) { mock }
+      let(:ticket) { 'ST-1foo' }
+      let(:service) { 'https://service.example.edu/' }
+      let(:cas_response) { '' }
 
+      before do
+        client.cas_url = 'https://cas.example.edu/'
+        client.ticket_validator = validator
+
+        stub_request(:any, /.*/)
+      end
+
+      it 'sends the ticket and service URL to the CAS server' do
+        validator.stub(:valid?)
+
+        client.valid_ticket?(ticket, service)
+
+        a_request(:get, 'https://cas.example.edu/serviceValidate').
+          with(:query => { 'ticket' => ticket, 'service' => service }).
+          should have_been_made.once
+      end
+
+      describe 'if the proxy callback URL is given' do
         before do
-          client.cas_url = 'https://cas.example.edu'
-          client.ticket_validator = validator
+          validator.stub(:valid?)
         end
 
-        it 'returns true if the ticket is valid' do
-          validator.should_receive(:valid?).with(ticket, service).and_return(true)
+        it 'sends the proxy callback URL to the CAS server' do
+          client.proxy_callback_url = 'https://cas.example.edu/proxy/'
+
+          client.valid_ticket?(ticket, service)
+
+          a_request(:get, 'https://cas.example.edu/serviceValidate').
+            with(:query => { 'ticket' => ticket, 'service' => service, 'pgtUrl' => client.proxy_callback_url }).
+            should have_been_made.once
+        end
+      end
+
+      describe 'return value' do
+        before do
+          stub_request(:get, client.service_validate_url).to_return(:body => cas_response)
+        end
+
+        it 'is true if the ticket is valid' do
+          validator.should_receive(:valid?).with(cas_response).and_return(true)
 
           client.valid_ticket?(ticket, service).should be_true
         end
 
-        it 'returns false if the ticket is invalid' do
-          validator.should_receive(:valid?).with(ticket, service).and_return(false)
+        it 'is false if the ticket is invalid' do
+          validator.should_receive(:valid?).with(cas_response).and_return(false)
 
           client.valid_ticket?(ticket, service).should be_false
+        end
+
+        it 'includes a proxy-granting ticket if the validator returns one' do
+          validator.should_receive(:valid?).with(cas_response).and_return([true, 'PGT-1foo'])
+
+          ok, pgt = client.valid_ticket?(ticket, service)
+
+          ok.should be_true
+          pgt.should == 'PGT-1foo'
         end
       end
     end
