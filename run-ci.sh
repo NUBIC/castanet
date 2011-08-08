e67ea2f351c5196385df8cc0053de65d68b48f7f
David Yip <yipdw@northwestern.edu>
2011-08-08 13:01:42 -0500

    CI script.

---
+++ run-ci.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Runs a CI build for castanet.  Assumes:
+#
+# 1)	RVM is installed in the build user's home directory and can be
+# 	activated by sourcing ~/.rvm/scripts/rvm.
+#
+# 2)	rvm_install_on_use_flag and rvm_create_on_use_flag are set.
+#
+# 3)	The Ruby environment to use is provided as the first argument.
+
+. ~/.rvm/scripts/rvm
+
+if [ -z $1 ]; then
+	echo "Ruby environment not given; aborting"
+	exit 1
+fi
+
+rvm use $1@castanet
+
+rake -f init.rakefile
+bundle exec rake udaeta:install_dependencies --trace
+bundle exec rake ci --trace
